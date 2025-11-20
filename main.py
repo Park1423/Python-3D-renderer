95bb28224437fe4a502d99fa09996fb0cd7f470f
Park1423 <thirdringdesign@hotmail.com>
2025-11-20 14:17:06 -0600

    Update main.py

--- main.py
+++ main.py
@@ -48,24 +48,32 @@ triangle10 = [[0,50,-70.71],[0,-50,-70.71],[-70.71,-50,0]]
 triangle11 = [[0,-50,-70.71],[0,50,-70.71],[70.71,50,0]]
 triangle12 = [[70.71,50,0],[0,-50,-70.71],[70.71,-50,0]]
 
-shape1geometry = [triangle1,triangle2,triangle3,triangle4,triangle5,triangle6,triangle7,triangle8,triangle9,triangle10,triangle11,triangle12]
-shape1pos = [0,10,150]
+cubegeometry = [triangle1,triangle2,triangle3,triangle4,triangle5,triangle6,triangle7,triangle8,triangle9,triangle10,triangle11,triangle12]
+
+shapes = [  # Add shapes to the scene with cube geometry and positions. positions are [x, y, z]
+        [cubegeometry,[0,10,150]],
+        [cubegeometry,[-200,10,200]],
+        ]
 
 #render
 
 def main():
-    #initilize here to avoid local variable error or something
+    # Initilize here to avoid local variable error or something
     current_time = time.perf_counter()
     last_time = time.perf_counter()
     delta = current_time - last_time
 
+    deltaaccumulator = 0
+    fpsacculumator = 0
+    fps = 0
+
     while True:
-        current_time = time.perf_counter() #Do delta/fps stuff
+        current_time = time.perf_counter() # Do delta/fps stuff
         delta = current_time - last_time
         last_time = current_time
-        print(str(1/delta))
+        
 
-        if keyboard.is_pressed("w"):    #get input and move camera.
+        if keyboard.is_pressed("w"):    # Get input and move camera. (Technically moves everything else)
             camerapos[2] += 400 * delta
         elif keyboard.is_pressed("s"):
             camerapos[2] -= 400 * delta
@@ -83,46 +91,47 @@ def main():
         if camerapos[1] > 100 and not keyboard.is_pressed("space"):
             camerapos[1] -= 500 * delta
 
-        cursor.clear() # clear the screen so we can put something new on it
-
-        for face in shape1geometry: #currently only working with one object, and we iterate for every face in the object
-            cursor.pu() #stop drawing for now
-            cursor.color("black",("#"+ str(random.randint(100000,999999))))
-            cursor.goto(point_to_screen(face[2],shape1pos)) #Go to the last point to ensure we fill the shape properly
-
-            cursor.begin_fill() #start filling the side
-            cursor.pd()
-
-            for point in face:
-
-                cursor.goto(point_to_screen(point,shape1pos))   #Go to each point of each face and connect the dots.
-                #cursor.dot(10,"red")   #uncomment this to see the points of the cube
-                
-            cursor.end_fill() # finish fill
-            cursor.pu()
-
+        cursor.clear() # Clear the screen so we can put something new on it
         
+        deltaaccumulator += delta # FPS calculations
+        fpsacculumator += 1
+        if deltaaccumulator >= 1:
+            fps = fpsacculumator
+            fpsacculumator = 0
+            deltaaccumulator = 0
+
+        cursor.pu()
+        cursor.goto(-300,300)
+        cursor.write(f"FPS: {str(round(fps))}") #Display FPS
+            
+        
+        for shape in shapes:
+            for face in shape[0]: #currently only working with one object, and we iterate for every face in the object
+                cursor.pu() #stop drawing for now
+                cursor.color("black",("#8D1E1E"))
+                cursor.goto(point_to_screen(face[2],shape[1])) #Go to the last point to ensure we fill the shape properly
+
+                cursor.begin_fill() #start filling the side
+                cursor.pd()
+
+                for point in face:
+
+                    cursor.goto(point_to_screen(point,shape[1]))   #Go to each point of each face and connect the dots.
+                    #cursor.dot(10,"pink")   # Uncomment this to see the points of the cube, but for some reason this practically DOUBLES lag.
+                    
+                cursor.end_fill() # finish fill
+                cursor.pu()
+
         screen.update() #force a screen update just to be safe
         
-        #This code has been commented out to have gradual movement, but you can uncomment it if you want. just make sure you comment the "shape1pos[0] += 0.1" line out
-        # shape1pos = input("The cube is at " + str(shape1pos) + ". Input new coordinates or enter stop to end the program: ")
-        # if shape1pos == "stop":
-        #     break
-        # else:
-        #     shape1pos = shape1pos.split(",")
-        #     shape1pos[0] = int(shape1pos[0])
-        #     shape1pos[1] = int(shape1pos[1])
-        #     shape1pos[2] = int(shape1pos[2])
-        #     cursor.clear()
-
         #Frame rate limiter
         #time.sleep(0.0005)
 
 def point_to_screen(pointpos,objectpos):
     distance = Focal_Length / (pointpos[2] + objectpos[2] - camerapos[2])   #Focal_Length / zpos works when nothing is moving, but here we have the point, camera, and objects positions in play
-    localx = pointpos[0] + objectpos[0] - camerapos[0]   #same here. we only need the x or y of the point normally but we have other things in play   
+    localx = pointpos[0] + objectpos[0] - camerapos[0]   #same here. we only need the x or y of the point normally, but we have other things in play   
     localy = pointpos[1] + objectpos[1] - camerapos[1]
-    return (distance * localx),(distance * localy)  # Final math. We could have done this all on one line, but I used local variables for readability.
+    return (distance * localx),(distance * localy)  # Final math. We could have done this whole function on one line, but I used local variables for readability.
 
 if __name__ == '__main__':
     main()
